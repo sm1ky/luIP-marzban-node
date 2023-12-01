319a821dc314307ffc8758ac1fefb455dcfee643
sm1ky <145751659+sm1ky@users.noreply.github.com>
2023-12-01 10:38:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,53 +4,74 @@ from auth import get_token
 import subprocess
 from utils import ban_ip, unban_ips
 from config import Config, load_config
+import logging
 
 config: Config = load_config()
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s "
+        "[%(asctime)s] - %(name)s - %(message)s",
+    )
+
+logging.getLogger("socketio").setLevel(logging.DEBUG)
+logging.getLogger("engineio").setLevel(logging.DEBUG)
 
 URL = f"{config.data.url}:{config.data.port}"
 
 socket_server_url = f"{URL}"
 
-api_key = get_token(url=URL, apipath=config.data.apipath, params={"username": f"{config.data.username}", "password": f"{config.data.password}"})
+api_key = get_token(url=URL, apipath=config.data.apipath, params={"username": f"{config.data.username}", "password": f"{config.data.password}"}) 
+
+if api_key is not None and 'data' in api_key and api_key['data'] is not None and 'api_key' in api_key['data']:
+    pass
+else:
+    logger.error('Invalid or missing api_key')
+
 
 sio = socketio.Client()
 
 @sio.event
 def connect():
-    print("Connected to the server")
+    logger.info(msg="Connected to the server")
 
 @sio.event
 def disconnect():
-    print("Disconnected from the server")
+    logger.info(msg="Disconnected from the server")
     
 @sio.event
 def connect_error(data):
-    print(f"Connection failed: {data}")
-
-    
-@sio.on("*")
-def any_event_handler(event, data):
-    print(f"Received event '{event}': {data}")
+    logger.info(msg=f"Connection failed: {data}")
 
 @sio.on("user:ip:ban")
 def on_unban(data):
+    logger.info(msg=f"Received ban event: {data}")
     ban_ip(data)
-    print("Received ban event:", data)
 
 @sio.on("user:ip:unban")
 def on_unban(data):
+    logger.info(msg=f"Received unban event: {data}")
     unban_ips()
-    print("Received unban event:", data)
-
-
+    
+@sio.on("*")
+def any_event_handler(event, data):
+    logger.info(msg=f"Received event '{event}': {data}")
 
 def main():
     while True:
         try:
-            sio.connect(f"{socket_server_url}", namespaces=[f'{config.data.socketpath}'], headers={"api_key": f"{api_key['data']['api_key']}"}) 
+            logger.info(f"Connecting to server with api_key: {api_key['data']['api_key']}")
+            sio.connect(
+                f"{socket_server_url}",
+                namespaces=[f'{config.data.socketpath}', '/'],
+                headers={"api_key": f"{api_key['data']['api_key']}"}
+            ) 
+            logger.info(f"Connected to server with api_key: {api_key['data']['api_key']}")
             sio.wait()
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(f"Error connecting to server: {e}")
             time.sleep(5)
 
 if __name__ == "__main__":
