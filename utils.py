759a1310dddfad1a5652b15ca794445599357dc4
Sm1ky <145751659+sm1ky@users.noreply.github.com>
2023-12-02 17:55:19 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,21 +14,21 @@ def ban_ip(params):
         try:
             result = subprocess.run(args, text=True, timeout=3)
             if result.returncode == 0:
-                logger.info(f"IP {ip} banned successfully.")
+                logging.info(f"IP {ip} banned successfully.")
             else:
-                logger.info(f"Failed to ban IP {ip}. | Error code: {result.returncode}")
+                logging.info(f"Failed to ban IP {ip}. | Error code: {result.returncode}")
         except subprocess.TimeoutExpired as e:
-            logger.info(f"IP {ip} banned successfully.")
+            logging.info(f"IP {ip} banned successfully.")
         except subprocess.CalledProcessError as e:
-            logger.info(f"Failed to ban IP {ip}.")
-            logger.info(f"Error: {e}")
-            logger.info(f"Script error output: {e.output}")
+            logging.logging(f"Failed to ban IP {ip}.")
+            logging.info(f"Error: {e}")
+            logging.info(f"Script error output: {e.output}")
     else:
         logging.error("'expireAt' should be a digit.")
         
 def unban_ips():
     try:
         subprocess.run(["bash", "scripts/ipunban.sh"])
-        logger.info("IPs unbanned successfully.")
+        logging.info("IPs unbanned successfully.")
     except subprocess.CalledProcessError as e:
-        logger.info("Error executing ipunban.sh:", e)
+        logging.info("Error executing ipunban.sh:", e)
