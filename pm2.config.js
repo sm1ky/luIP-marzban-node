877848dd97699256cf38b7fc129f33515cff4f5f
Sm1ky <145751659+sm1ky@users.noreply.github.com>
2023-12-03 14:43:55 +0200

    log 10mb

--- pm2.config.js
+++ pm2.config.js
@@ -11,6 +11,9 @@ module.exports = {
         log_date_format: "YYYY-MM-DD HH:mm Z",
         out_file: "logs/out.log",
         error_file: "logs/error.log",
+        merge_logs: true,
+        max_size: "10M",
+        max_files: 5,
       },
     ],
   };
